b6557135e4653ff68fbc784a2bd4ed60914ffe7f
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-03-28 23:41:45 -0500

    Revert "Added another reference to new method"
    This reverts commit e54ac0c4db01fdf3a889cdeb413f56746d41291d.

--- lib/dota.rb
+++ lib/dota.rb
@@ -4,7 +4,7 @@ require 'dota/client'
 module Dota
   extend SingleForwardable
 
-  def_delegators :client, :configure, :match, :history, :history_by_sequence_number, :leagues, :live_leagues, :profiles, :bans, :friends
+  def_delegators :client, :configure, :match, :history, :leagues, :live_leagues, :profiles, :bans, :friends
 
   def self.client
     @client ||= Client.new
